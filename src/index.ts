5fa9955d9dd5d21a620549c75d65b422f78f00bb
Santos-Samuels <saamsmith15@gmail.com>
2024-02-26 00:20:21 -0300

    refactor: handle with cors and refactor input model

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Request, Response, Router } from "express";
 import OpenAI from "openai";
 import { foo3 } from "./foo/foo";
@@ -15,6 +16,7 @@ const app = express();
 const route = Router();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const formmatHistory = (history: string): GPTHistory => {
@@ -30,51 +32,58 @@ route.get("/", async (req: Request, res: Response) => {
 });
 
 route.post("/chat", async (req: Request, res: Response) => {
-  const { theme } = req.body;
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: `Crie uma história na qual acontece um dialógo entre dois personagens sobre o tema ${theme}.
-    - O diálogo deve ser não linear, ambos podem fazer perguntas ou afirmativas sobre o tema.
-    - Deve haver 20 cenasa;
-    - O retorno é no formato .json;
-    - O Json deve incluir TODAS as cenas;
-    - Deve incluir 4 pilares desse tema em "ramifications";
-    
-    A saida deve respeitar o seguinte formato:
-        export type EmotionTypes =
-          | "happy"
-          | "sad"
-          | "surprised"
-          | "thinking"
-          | "confused"
-          | "very-happy"
-          | "neutral";
-        
-        export type ScenePosition = "left" | "right";
-    
-        export interface IHistory {
-          theme: string;
-          resume: string;
-          ramifications?: string[];
-        }
-       
-        export interface IScene {
-          speech: string;
-          emotion: EmotionTypes;
-          position: ScenePosition;
-        }`,
-      },
-    ],
-    model: OPENAI_MODEL,
-  });
-
-  const formattedHistory: GPTHistory = formmatHistory(
-    completion.choices[0].message.content || ""
-  );
-
-  res.json(formattedHistory);
+  try {
+    const { theme } = req.body;
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `Crie uma história na qual acontece um dialógo entre dois personagens sobre o tema ${theme}.
+          
+          Critérios de aceite:
+          - O diálogo deve ser não linear, ambos podem fazer perguntas ou afirmativas sobre o tema.
+          - Deve haver 20 cenasa;
+          - Deve incluir 4 pilares desse tema em "ramifications";
+          - Resumo da história deve ser em "resume";
+          - O retorno é no formato .json;
+          - O Json deve incluir TODAS as cenas;
+      
+      A saida deve respeitar o seguinte formato:
+          export type EmotionTypes =
+            | "happy"
+            | "sad"
+            | "surprised"
+            | "thinking"
+            | "confused"
+            | "very-happy"
+            | "neutral";
+          
+          export type ScenePosition = "left" | "right";
+      
+          export interface IHistory {
+            theme: string;
+            resume: string;
+            ramifications?: string[];
+          }
+         
+          export interface IScene {
+            speech: string;
+            emotion: EmotionTypes;
+            position: ScenePosition;
+          }`,
+        },
+      ],
+      model: OPENAI_MODEL,
+    });
+  
+    const formattedHistory: GPTHistory = formmatHistory(
+      completion.choices[0].message.content || ""
+    );
+  
+    res.json(formattedHistory);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.use(route);
